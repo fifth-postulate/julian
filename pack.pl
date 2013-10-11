a55596dcf6b84f4a5565136abae3365708de11f0
Michael Hendricks <michael@ndrix.org>
2013-10-11 08:31:27 -0600

    Tidy pack file

--- pack.pl
+++ pack.pl
@@ -1,8 +1,10 @@
 name(julian).
 version('0.0.1').
-title('').
+title('Date and time library').
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
+
 home('http://packs.ndrix.com/julian/index.html').
 download('http://packs.ndrix.com/julian/julian-0.0.1.tgz').
